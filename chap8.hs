779e3c5e9287b352c35310cb2959a02d233297ce
IAmPara0x <confusedstudent13@gmail.com>
2022-05-14 19:12:30 +0530

    complete chapter 10

--- chap8.hs
+++ chap8.hs
@@ -2,12 +2,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 
 import           Prelude hiding (Either(..))
 import           Data.Coerce   (Coercible(..), coerce)
 import           Data.Foldable (toList)
 import           Data.Monoid   (Sum(..), Product(..))
+import           GHC.TypeLits
 
 import qualified Data.Map as M
 
@@ -28,5 +34,21 @@ type family IntToBool (a :: *) :: * where
   IntToBool Int = Bool
   IntToBool a   = a
 
+type Names = [Symbol]
+
+name :: Symbol -> String
+name = undefined
+
 main :: IO()
 main = print "Yuno"
+
+type Theme = [Symbol]
+
+class HasColor (color :: Symbol) (theme :: Theme)
+
+instance HasColor color (color : rest)
+instance {-# Overlappable #-}
+  HasColor color rest => HasColor color (color' : rest)
+
+testFunction :: HasColor color theme => ()
+testFunction = ()
