18219000faa65d5e4a8def96681ec1d6a3c40e38
IAmPara0x <confusedstudent13@gmail.com>
2022-05-28 19:58:32 +0530

    complete chapter 12

--- product.hs
+++ product.hs
@@ -1,19 +1,19 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes      #-}
+{-# LANGUAGE ConstraintKinds          #-}
+{-# LANGUAGE DataKinds                #-}
+{-# LANGUAGE FlexibleContexts         #-}
+{-# LANGUAGE FlexibleInstances        #-}
+{-# LANGUAGE GADTs                    #-}
+{-# LANGUAGE MultiParamTypeClasses    #-}
+{-# LANGUAGE OverloadedLabels         #-}
+{-# LANGUAGE PolyKinds                #-}
+{-# LANGUAGE RankNTypes               #-}
+{-# LANGUAGE ScopedTypeVariables      #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeApplications         #-}
+{-# LANGUAGE TypeFamilies             #-}
+{-# LANGUAGE TypeOperators            #-}
+{-# LANGUAGE UndecidableInstances     #-}
 
 import Data.Functor.Identity
 import Data.Kind (Constraint, Type)
@@ -36,10 +36,38 @@ data OpenProduct f ts where
 type Key :: Symbol -> Type
 data Key key = Key
 
+instance (key ~ key') => IsLabel key (Key key') where
+  fromLabel = Key
+
 type Dict = OpenProduct Identity
 
 type UniqueKey (key :: k) (ts :: [(k, t)]) =
   Null =<< Filter (TyEq key <=< Fst) ts
+
+type family
+  RequireUniqueKey
+    (result :: Bool)
+    (key :: Symbol)
+    (t :: k)
+    (ts :: [(Symbol, k)]) ::
+    Constraint
+  where
+  RequireUniqueKey 'True key t ts  = ()
+  RequireUniqueKey 'False key t ts =
+    TypeError
+      (   'Text "Attempting to add a field named `"
+    ':<>: 'Text key
+    ':<>: 'Text "' with type "
+    ':<>: 'ShowType t
+    ':<>: 'Text "to an OpenProduct."
+    ':$$: 'Text "But the OpenProduct already has a field `"
+    ':<>: 'Text key
+    ':<>: 'Text "' with type "
+    ':<>: 'ShowType (Eval (LookupType key ts))
+    ':$$: 'Text "Consider using `update' "
+    ':<>: 'Text "instead of `insert' ."
+    ':$$: 'Text ""
+      )
 
 type LookupType (key :: s) (ts :: [(s, t)]) = (LookupTypeImpl key ts)
 
@@ -47,7 +75,17 @@ data LookupTypeImpl :: s -> [(s, t)] -> Exp t
 
 type instance Eval (LookupTypeImpl k ts) = Eval (FromMaybe Stuck =<< Lookup k ts)
 
-type FindElem (key :: k) (ts :: [(k, t)]) = FromMaybe Stuck =<< FindIndex (TyEq key <=< Fst) ts
+type family FindElem (key :: k) (ts :: [(k, t)]) where
+  FindElem key ts =
+    FromMaybe
+      ( TypeError
+          ( 'Text "There's no such key with the name of "
+              ':<>: 'ShowType key
+              ':<>: 'Text " in the dictionary of type"
+              ':$$: 'ShowType ts
+          )
+      )
+      =<< FindIndex (TyEq key <=< Fst) ts
 
 type UpdateElem (key :: k) (t :: m) (ts :: [(k, m)]) =
   SetIndex (Eval (FindElem key ts)) '(key, t) ts
@@ -91,7 +129,7 @@ nil = OpenProduct V.empty
 
 insert' ::
   forall key f t ts.
-  (Show (f t), Eval (UniqueKey key ts) ~ True) =>
+  (Show (f t), RequireUniqueKey (Eval (UniqueKey key ts)) key t ts) =>
   Key key ->
   f t ->
   OpenProduct f ts ->
@@ -144,7 +182,7 @@ upsert' _ ft (OpenProduct v) = OpenProduct $
 
 insert ::
   forall key t ts.
-  (Show (Identity t), Eval (UniqueKey key ts) ~ True) =>
+  (Show (Identity t), RequireUniqueKey (Eval (UniqueKey key ts)) key t ts) =>
   t ->
   OpenProduct Identity ts ->
   OpenProduct Identity ('(key, t) ': ts)
@@ -228,7 +266,7 @@ class Inserts (keys :: [Symbol]) (f :: (Type -> Type)) (xs :: [Type]) (ts :: [(S
     OpenProduct f ts ->
     OpenProduct f (InsertsType keys xs ts)
 
-instance Inserts '[]  f '[] ts where
+instance Inserts '[] f '[] ts where
   inserts' _ _ p = p
 
 instance
@@ -236,7 +274,8 @@ instance
     Eval (UniqueKey k (InsertsType ks xs ts)) ~ True,
     Inserts ks f xs ts
   ) =>
-  Inserts (k ': ks) f (x ': xs) ts where
+  Inserts (k ': ks) f (x ': xs) ts
+  where
   inserts' _ (ft :# fts) p = insert' @k @f @x Key ft (inserts' @ks @f @xs Keys fts p)
 
 gets ::
@@ -249,19 +288,35 @@ gets = gets' @keys @_ @Identity Keys
 inserts ::
   forall keys xs ts.
   (Inserts keys Identity xs ts) =>
-  HList (Apply Identity xs) ->
+  HList xs ->
   OpenProduct Identity ts ->
   OpenProduct Identity (InsertsType keys xs ts)
+inserts = inserts' @keys @Identity @xs @ts Keys . hmap @Identity
 
-inserts = inserts' @keys @Identity @xs @ts Keys
-  
+dict =
+  inserts @'["yuno", "miku", "ints", "bool"]
+    @'[String, String, [Int], Bool]
+    ("Yuno Gasai" :# "Miku Nanako" :# [1 .. 10] :# True :# HNil)
+    nil
 
-x = insert @"yuno" @String "Yuno Gasai" nil
+class Hmap (f :: Type -> Type) where
+  hmap :: forall ts. HList ts -> HList (Apply f ts)
 
-y = insert @"ints" @[Int] [1 .. 10] x
+instance Hmap Identity where
+  hmap HNil = HNil
+  hmap (t :# ts) = Identity t :# hmap @Identity ts
 
-z = insert @"bool" True $ insert @"rem" @String "Rem" y
-
+instance
+  ( TypeError
+      ( Text "Attempting to show a function of type `"
+          :<>: ShowType (a -> b)
+          :<>: Text "`"
+          :$$: Text "Did you foget to apply an argument?"
+      )
+  ) =>
+  Show (a -> b)
+  where
+  show = undefined
 
 main :: IO ()
-main = print (gets @'["yuno", "rem"] z)
+main = print (get' #yuno dict)
